ed756828c3c4caad17a0a6e36ccf0e1d51645d8d
Vladyslav Pihol <freepgvlad@gmail.com>
2025-11-06 19:44:04 +0200

    Fixed changes for BookViewSet

--- service_book/views.py
+++ service_book/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from rest_framework import viewsets
 
-# Create your views here.
+from service_book.models import Book
+from service_book.serializers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
