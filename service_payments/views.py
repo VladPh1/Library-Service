39836165fb98ca050eacf687b219209408f485c2
Vladyslav Pihol <freepgvlad@gmail.com>
2025-11-08 13:12:19 +0200

    aded PaymentViewSet to service_payments/views.py

--- service_payments/views.py
+++ service_payments/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from service_payments.models import Payment
+from service_payments.serializers import PaymentSerializer
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    serializer_class = PaymentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_staff:
+            queryset = Payment.objects.all()
+        else:
+            queryset = Payment.objects.filter(borrowing__user=user)
+        return queryset.select_related("borrowing", "borrowing__user")
