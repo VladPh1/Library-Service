4300a7cfcd7259cecda1ce2c2d16d824745ed3fa
Vladyslav Pihol <freepgvlad@gmail.com>
2025-11-08 12:54:20 +0200

    aded model Payment to service_payments/models.py

--- service_payments/models.py
+++ service_payments/models.py
@@ -1,3 +1,39 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from service_borrowing.models import Borrowing
+
+
+class Payment(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = "PENDING"
+        PAID = "PAID"
+        EXPIRED = "EXPIRED"
+
+    class TypeChoices(models.TextChoices):
+        PAYMENT = "PAYMENT"
+        FINE = "FINE"
+
+    status = models.CharField(
+        max_length=7,
+        choices=StatusChoices.choices,
+        default=StatusChoices.PENDING,
+    )
+    type = models.CharField(
+        max_length=7,
+        choices=TypeChoices.choices,
+        default=TypeChoices.PAYMENT,
+    )
+    borrowing = models.ForeignKey(
+        Borrowing, on_delete=models.CASCADE, related_name="payments"
+    )
+    session_url = models.URLField(max_length=200)
+    session_id = models.CharField(max_length=255, blank=True, null=True)
+    money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return (
+            f"[{self.get_status_display()}] {self.get_type_display()} "
+            f"Borrow: #{self.borrowing.id}"
+        )
