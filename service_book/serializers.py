67d2b2fa1ac110e3a382fc9541bbf8573118c788
Vladyslav Pihol <freepgvlad@gmail.com>
2025-11-08 11:25:47 +0200

    add BorrowingSerializer to service_borrowing/serializers.py

--- service_book/serializers.py
+++ service_book/serializers.py
@@ -6,4 +6,8 @@ from service_book.models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ("title", "author", "cover", "inventory", "daily_fee")
+        fields = ("title",
+                  "author",
+                  "cover",
+                  "inventory",
+                  "daily_fee")
---
+++ service_borrowing/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from service_borrowing.models import Borrowing
+
+
+class BorrowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user"
+        )
