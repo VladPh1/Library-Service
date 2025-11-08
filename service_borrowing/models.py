de29346fbb6424fb993a7760f1514fa300f567c9
Vladyslav Pihol <freepgvlad@gmail.com>
2025-11-08 11:58:17 +0200

    fixed changes to service_borrowing/models.py

--- service_borrowing/models.py
+++ service_borrowing/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 from service_book.models import Book
@@ -5,16 +6,29 @@ from user.models import User
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateField()
+    borrow_date = models.DateField(
+        auto_now_add=True,
+    )
     expected_return_date = models.DateField()
-    actual_return_date = models.DateField()
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    actual_return_date = models.DateField(
+        null=True,
+        blank=True,
+    )
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowing")
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowing"
+    )
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(expected_return_date__gt=models.F("borrow_date")),
+                name="expected_return_date_must_be_after_borrow_date",
+            )
+        ]
 
     def __str__(self):
         return (
-            f"Book: {self.book},"
-            f"Borrow date: {self.borrow_date},"
-            f"Expected date: {self.expected_return_date},"
-            f"Actual return date: {self.actual_return_date}"
+            f"Book: {self.book.title},"
+            f"Borrow: {self.user.email} - {self.borrow_date}"
         )
