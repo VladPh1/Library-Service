deb0c973d8cbebfc7c436072d1287cd29c427f7f
Vladyslav Pihol <freepgvlad@gmail.com>
2025-11-09 15:34:25 +0200

    aded success & cancel to PaymentViewSet from service_payment/views.py

--- service_payments/views.py
+++ service_payments/views.py
@@ -1,5 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+import stripe
 
 from service_payments.models import Payment
 from service_payments.serializers import PaymentSerializer
@@ -17,3 +20,39 @@ class PaymentViewSet(viewsets.ModelViewSet):
         else:
             queryset = Payment.objects.filter(borrowing__user=user)
         return queryset.select_related("borrowing", "borrowing__user")
+
+    @action(detail=False, methods=["get"], url_path="success")
+    def success(self, request):
+        session_id = request.query_params.get("session_id")
+
+        try:
+            session = stripe.checkout.Session.retrieve(session_id)
+
+            if session.payment_status == "paid":
+
+                payment = Payment.objects.get(session_id=session_id)
+                payment.status = Payment.StatusChoices.PAID
+                payment.save()
+
+                return Response(
+                    {"message": f"Payment {payment.id} successful!"},
+                    status=status.HTTP_200_OK,
+                )
+            else:
+                return Response(
+                    {"message": "Payment not successful."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+        except Payment.DoesNotExist:
+            return Response(
+                {"error": "Payment not found."}, status=status.HTTP_404_NOT_FOUND
+            )
+        except Exception as e:
+            return Response(
+                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+    @action(detail=False, methods=["get"], url_path="cancel")
+    def cancel(self, request):
+        return Response({"message": "Payment cancel."})
