3ff06bd69b56de7f94f8e8ad346b1a26d40d6a70
Vladyslav Pihol <freepgvlad@gmail.com>
2025-11-11 13:14:52 +0200

    aded permission_classes to BookViewSet from service_book/views.py

--- service_book/views.py
+++ service_book/views.py
@@ -1,10 +1,13 @@
 from django.views.generic import ListView
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from service_book.models import Book
+from service_book.permissions import IsAdminOrIfAuthenticatedReadOnly
 from service_book.serializers import BookSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
